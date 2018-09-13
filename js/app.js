c769106b3482395f0d9b79931de3cf8bd8818fa3
Scott Currie <vitriolik@gmail.com>
2018-09-12 17:01:13 -0700

    improved console logging

--- js/app.js
+++ js/app.js
@@ -21,12 +21,14 @@ var invalidResponse = 'Sorry. I couldn\'t understand your response.';
 var answer = '';
 var responseData = [];
 var response = '';
+var msg = '';
 var correctAnswers = 0;
+
 // Loop through all the questions
 for (var i = 0; i < questions.length; i++) {
   answer = prompt(questions[i]);
+  console.log('Question: ' + i + ', user answer: ' + answer);
   answer = answer.toLowerCase();
-  console.log('Question: ' + i + ', user answer' + answer);
   // Turn user's answer into a 'y', 'n', or 'q'
   if ((answer === 'y') || (answer === 'yes')) {
     answer = 'y';
@@ -40,6 +42,7 @@ for (var i = 0; i < questions.length; i++) {
   // find out if answer is 'q'. That means we know our response already.
   if (answer === 'q') {
     response = invalidResponse;
+    msg = 'Invalid answer. Correct answers so far: ';
   }
   // do the following if answer was not 'q'
   else {
@@ -47,20 +50,22 @@ for (var i = 0; i < questions.length; i++) {
     if (answer === responseData[0]) {
       response = responseData[1];
       correctAnswers++;
-      console.log('Correct answer. Correct answers so far: ' + correctAnswers);
+      msg = 'Correct answer. Correct answers so far: ';
+      // console.log('Correct answer. Correct answers so far: ' + correctAnswers);
     }
     // if it wasn't q and wasn't the key, then response is responseData[2]
     else {
       response = responseData[2];
-      console.log('Incorrect answer. Correct answers so far: ' + correctAnswers);
+      // console.log('Incorrect answer. Correct answers so far: ' + correctAnswers);
+      msg = 'Incorrect answer. Correct answers so far: ';
     }
   }
+  console.log(msg + correctAnswers);
   alert(response);
 }
 // Number guessing logic
 alert('Let\'s play a guessing game. I\'ll think of a number between 1 and 10, and you\'ll try to guess it.');
 var targetNum = Math.random() * 10;
-console.log(targetNum);
 // This part is not great, but it works to keep the numbers in range without having to learn the right way or copypasta some code 
 if (targetNum >= 0 && targetNum < 1.0) {
   targetNum = 1;
@@ -70,14 +75,16 @@ if (targetNum >= 0 && targetNum < 1.0) {
 else {
   targetNum = Math.floor(targetNum);
 }
-console.log(targetNum);
+console.log('Number to guess: ' + targetNum);
 var tries = 4;
-var msg = '';
+// var msg = '';
 while (tries > 0) {
   var guess = prompt('What\'s your guess?');
+  console.log('User guess: ' + guess);
   if (parseInt(guess) === targetNum) {
     alert('Nice! You guessed it.');
     correctAnswers++;
+    console.log('Number guessing game won. Correct answers so far: ' + correctAnswers);
     break;
   } else if (guess < targetNum) {
     // alert('No. Your guess was too low. Try again.');
@@ -92,9 +99,11 @@ while (tries > 0) {
   }
   tries--;
   if (tries === 0) {
+    console.log('Number guessing game failed. Correct answers so far: ' + correctAnswers);
     alert(msg + '\nSorry. You\'re out of tries. My number was ' + targetNum);
   }
   else {
+    console.log('Incorrect guess. Tries remaining: ' + tries);
     alert(msg + ' Try again.');
   }
 }
